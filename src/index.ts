fd7436721cd9adbcb80f794ec9ed639c02ae75de
devlsl <vslozhen@gmail.com>
2024-12-02 21:53:10 +0300

    exported ApiClientOptions type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { createReactUseApi } from './client/createReactUseApi';
 export { createApiClient } from './client/createApiClient';
 export { requestHandlerBuilder } from './requestHandlerBuilder';
+export { ApiClientOptions } from './client/types';
 export { ActionError } from './ActionError';
 export type { HandlersTypeGenerator } from './HandlersTypeGenerator';
 export type {
